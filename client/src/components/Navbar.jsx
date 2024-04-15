03b00f28eb117b4489cfe0a0cfa460b4e215fb74
Ayush Kumar Gupta <guptayush47@gmail.com>
2024-04-15 15:00:34 +0530

    tailwind instalation and components created

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <header>
+        <nav className='to-blue-900 h-4 flex'>
+            <img src="" alt="" />
+            <div>
+                <input type="text" />
+            </div>
+            <div>
+                Hello boy
+            </div>
+        </nav>
+    </header>
+  )
+}
+
+export default Navbar
