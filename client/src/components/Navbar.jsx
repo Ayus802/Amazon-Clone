89e0b2c11a3a39a6bc7a32a3610ff82b974dbdae
Ayush Kumar Gupta <guptayush47@gmail.com>
2024-04-26 01:46:22 +0530

    css

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,12 +3,13 @@ import React from 'react'
 function Navbar() {
   return (
     <header >
-        <nav className='bg-blue-950 h-14 flex top-0 w-screen fixed'>
+        <nav className='bg-blue-950 h-14 flex top-0 w-screen fixed justify-between items-center'>
             <img src="" alt="" />
-            <div>
-                <input type="text" />
+            <div className='w-1/4 h-2/3 flex'>
+                <input type="text" className='w-full rounded-l-full h-full pl-4 text-xl border-none'/>
+                <button type="submit" className='bg-slate-600'>Search</button>
             </div>
-            <div>
+            <div className='mr-8'>
                 Hello boy 
             </div>
         </nav>
